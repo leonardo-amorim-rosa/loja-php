4193d66fc0973a55a343f6adad20b7e5df0085e5
leonardo <leoamorimrosa@gmail.com>
2015-09-08 11:07:29 -0300

    segurança contra sql injection

--- banco-produtos.php
+++ banco-produtos.php
@@ -1,13 +1,15 @@
 <?php
 
 		function cadastraProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado) {
+			$nome = mysqli_real_escape_string($conexao, $nome);
+			$descricao = mysqli_real_escape_string($conexao, $descricao);
 			$query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id}, {$usado})";
 			return mysqli_query($conexao, $query);
 		}
 
 		function listaProdutos($conexao) {
 			$produtos = array();
-			$resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id");
+			$resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id order by p.id");
 			while($produto = mysqli_fetch_assoc($resultado)) {
 				array_push($produtos, $produto);
 			}
@@ -26,6 +28,8 @@
 		}
 
 		function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado) {
+			$nome = mysqli_real_escape_string($conexao, $nome);
+			$descricao = mysqli_real_escape_string($conexao, $descricao);
 			$query = "update produtos set nome='{$nome}', preco={$preco}, descricao='{$descricao}', categoria_id={$categoria_id}, usado={$usado} where id={$id}";
 			return mysqli_query($conexao, $query);
 		}
--- banco-usuarios.php
+++ banco-usuarios.php
@@ -1,6 +1,8 @@
 <?php
   function buscaUsuario($conexao, $email, $senha) {
     $senhaMD5 = md5($senha);
+    $email = mysqli_real_escape_string($conexao, $email);
+    $senha = mysqli_real_escape_string($conexao, $senha);
     $query = "select * from usuarios where email='{$email}' and senha='{$senhaMD5}'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
